fed35b179352bf937a219a0625ffb670ef11d812
Pablo Monti <Pablo_Monti@hotmail.com.ar>
2023-02-25 20:20:17 -0300

    pagina de login

--- Shop/src/app/app.module.ts
+++ Shop/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
