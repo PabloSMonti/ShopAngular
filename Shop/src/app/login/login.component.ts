3b7b5c8384e413539514f1d2f17e8a8313664e5e
Pablo Monti <Pablo_Monti@hotmail.com.ar>
2023-02-25 22:47:28 -0300

    refactor login component

--- Shop/src/app/login/login.component.ts
+++ Shop/src/app/login/login.component.ts
@@ -7,31 +7,38 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  
   hide = true;
+
   public usrMinLength : number = 5;
   public pwsMinLength : number = 8;
+
   form: FormGroup = new FormGroup({
     username: new FormControl('',[Validators.required,Validators.minLength(this.usrMinLength)]),
     password: new FormControl('',[Validators.required,Validators.minLength(this.pwsMinLength)]),
   });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   submit() {
+
     if (this.form.valid) {
-      this.submitEM.emit(this.form.value);
+      this.submitLoginEM.emit(this.form.value);
     }
+
   }
+
   @Input() error!: string;
 
-  @Output() submitEM = new EventEmitter();
+  @Output() submitLoginEM = new EventEmitter();
 
 
   get username() { return this.form.get('username'); }
 
-get password() { return this.form.get('password'); }
+  get password() { return this.form.get('password'); }
 }
 
 
